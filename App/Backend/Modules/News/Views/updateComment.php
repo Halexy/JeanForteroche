e6446ca2142356376f533e908c2a2d8fa6ef9961
hajjar <alexyhajjar.ch@gmail.com>
2020-09-16 15:59:40 +0200

    test

---
+++ App/Backend/Modules/News/Views/updateComment.php
@@ -0,0 +1,21 @@
+<div class="wrapper fadeInDown">
+  <div class="formContent">
+    <!-- Tabs Titles -->
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="/images/update-comment-icon.PNG" class="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login Form -->
+    <h2>Modifier un commentaire</h2>
+      <form action="" method="post">
+        <p>
+          <?= $form ?>
+      
+          <input type="submit" value="Modifier" />
+        </p>
+      </form>
+
+  </div>
+</div>
